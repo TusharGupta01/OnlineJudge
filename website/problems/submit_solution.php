ddd0e82b38cc5bc02e5b834499419674b1766db6
gopal <gopalkriagg@gmail.com>
2015-12-01 00:38:41 +0530

    Corrected hyperlinks in submit_soultion.php

--- website/problems/submit_solution.php
+++ website/problems/submit_solution.php
@@ -23,12 +23,31 @@
 </head>
 
 <body>
-
+<nav class="navbar " role="navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" style=" font-size:30px">Online Judge</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a>Welcome <?php echo $_SESSION["username"]; ?>!</a></li>
+        <li><a href='../index.php'>Dashboard</a></li>
+        <li><a href='../logout.php'> Log Out</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<hr>
 <?php
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    include 'header.php';
 ?>
 
 <div  align="center">
